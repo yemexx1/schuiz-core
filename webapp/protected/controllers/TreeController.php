9d97a05f7260035f214ac19b1243dc49c4144ad7
larikraun <larryade92@ymail.com>
2014-11-06 03:20:02 +0100

    Added additional Models and Controllers

--- webapp/protected/controllers/TreeController.php
+++ webapp/protected/controllers/TreeController.php
@@ -12,14 +12,17 @@ class TreeController extends CController
 
     public function actiongettree()
     {
+        header('Access-Control-Allow-Origin: *');
         $faculties = FacultyModel::model()->getAllFaculties();
         foreach ($faculties as &$faculty) {
             $departments = DepartmentModel::model()->getDepartments($faculty['id']);
             foreach ($departments as &$department) {
                 $courses = CourseModel::model()->getCourses($department['id']);
+
                 $department['num_childs'] = count($courses);
                 $department['childs'] = $courses;
             }
+
             $faculty['num_childs'] = count($departments);
             $faculty['childs'] = $departments;
         }
